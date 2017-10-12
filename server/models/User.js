5ba0d4c3f804f4e2741467471987fe5f9158d38b
hacktiv8 <hacktiv8@hacktiv8s-Mac-mini-7.local>
2017-10-12 14:09:15 +0700

    update backend create models DONE

--- server/models/User.js
+++ server/models/User.js
@@ -3,4 +3,18 @@
 const mongoose = require('mongose')
 const Schema = mongoose.Schema
 
+const userSchema = new Schema({
+    username:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
 
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
